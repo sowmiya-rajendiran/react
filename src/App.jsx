e391d45b456dbed9ebcb380baf4cdab6c25c35a4
Sowmiya Rajendiran <sowmiraj910@gmail.com>
2025-06-20 19:35:58 +0530

    First component

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// First Component
+function App(){
+  return (
+    // <div> 
+    //   <h1>Hello React !</h1>
+    //   <p>This is Para</p>
+    // </div>
+
+    // This is a fragment tag for grouping
+    <> 
+      <h1>Hello React !</h1>
+      <p>This is Para</p>
+      <h2>Using Fragment Tag</h2>
+    </>
+    
+  )
+}
+
+export default App;
