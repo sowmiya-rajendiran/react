6ea850b84fe54ccaff1a1f278673157c9ec2096c
Sowmiya Rajendiran <sowmiraj910@gmail.com>
2025-06-30 22:28:33 +0530

    useReducer for Forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,57 @@
 import { useReducer } from "react";
 
-import reducer from "../reducer";
+function reducer(state , action){
+    switch (action.type){
+        case 'Name' :
+            return {
+                ...state ,
+                name : action.payload
+            }
+        case 'Email' :
+            return {
+                ...state ,
+                email : action.payload
+            }
+        case 'Password' :
+            return {
+                ...state ,
+                password : action.payload
+            }
+    }
+}
 
 function App(){
-    let [state,dispatch] = useReducer(reducer , 0);
 
-    let handleIncrese = () =>{
-        dispatch ({type : 'Increment' , payload : 5});
-    }
-    let handleDecrese = () => {
-        dispatch({type : 'Decrement'});
-    }
-    let handleRest = () =>{
-        dispatch({type : 'Reset'})
-    }
+    let [state , dispatch] = useReducer(reducer , {
+        name :'',
+        email : '',
+        password :''
+    })
 
+    let handleSubmit = (event) =>{
+        event.preventDefault();
+        console.log(state);
+    }
+    let changeName = (event) =>{
+        dispatch({type : 'Name' , payload : event.target.value})
+    }
+    let changeEmail = (event) =>{
+        dispatch({type : 'Email' , payload : event.target.value})
+
+    }
+    let changePassword = (event) =>{
+        dispatch({type : 'Password' , payload : event.target.value})
+
+    }
     return(
         <>
-            <h1>This is value {state} </h1>
-            <button onClick={handleIncrese}>Click</button>
-            <button onClick={handleDecrese}>Click</button>
-            <button onClick= {handleRest}>Reset</button>
+            <h1>Register Form</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="name" value={state.name} onChange={changeName}></input><br></br><br></br>
+                <input type="email" placeholder="email" value={state.email} onChange={changeEmail}></input><br></br><br></br>
+                <input type="password" placeholder="password" value={state.password} onChange={changePassword}></input><br></br><br></br>
+                <button type="submit">Submit</button>
+            </form>
         </>
     )
 
