fcf2f84ecc0bfbddf663af230bece1420eb0b29f
Sowmiya Rajendiran <sowmiraj910@gmail.com>
2025-07-05 13:26:26 +0530

    Practise

--- reducer.js
+++ reducer.js
@@ -1,16 +1,21 @@
 function reducer(state , action){
-    console.log(action)
-    switch(action.type){
-        case 'Increment' : 
-            return state+ (action.payload) ;
-        case 'Decrement' :
-            return state -1 ;
-        case 'Reset' :
-            return 0 ;
-        default:
-            return 0;
+    switch (action.type){
+        case 'Name' :
+            return {
+                ...state ,
+                name : action.payload
+            }
+        case 'Email' :
+            return {
+                ...state ,
+                email : action.payload
+            }
+        case 'Password' :
+            return {
+                ...state ,
+                password : action.payload
+            }
     }
-
 }
 
 export default reducer;
